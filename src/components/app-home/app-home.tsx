5f328bc8c23f0ab641895ead4e0bf1544dff781a
Andrew Decker <ajdecker1022@gmail.com>
2019-02-05 04:53:36 -0600

    Add top and bottom tet

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -14,13 +14,21 @@ export class AppHome {
       </ion-header>,
 
       <ion-content padding>
-        <p>Coming soon to a website near you.</p>
-
         <ion-grid>
+          <ion-row>
+            <ion-col>
+              <h1 class="zenpylon-header" text-center text-uppercase>
+                zen pylon
+              </h1>
+            </ion-col>
+          </ion-row>
           <ion-row class="logo-row" justify-content-center>
             <ion-col text-center size="6">
               <img src="/assets/logo.jpg" />
             </ion-col>
+          </ion-row>
+          <ion-row text-center class="values-row">
+            <ion-col>Code, Creativity, Curiosity</ion-col>
           </ion-row>
         </ion-grid>
       </ion-content>
