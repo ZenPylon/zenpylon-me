44462943c84348c6056ed218f62aa7d40bb9d4d9
Andrew Decker <ajdecker1022@gmail.com>
2019-02-08 11:35:39 -0600

    Add paragraph styling and about placeholder template

--- src/components/app-about/app-about.tsx
+++ src/components/app-about/app-about.tsx
@@ -9,7 +9,18 @@ export class AppAbout {
     return [
       <app-header />,
       <ion-content>
-        <p>About</p>
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <h1>Photo here</h1>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col size-lg="7" size-xs="12">
+              <p>bio here</p>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
       </ion-content>,
     ];
   }
