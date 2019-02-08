8b845103f6f554a50ffe62a2f9979f0af72b0277
Andrew Decker <ajdecker1022@gmail.com>
2019-02-08 08:05:05 -0600

    Add conditional header display

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -5,22 +5,36 @@ import { Component } from '@stencil/core';
 })
 export class AppHeader {
   render() {
+    const isSmallScreen = document.documentElement.clientWidth < 700;
+    if (isSmallScreen) {
+      return (
+        <ion-header>
+          <ion-toolbar color="dark">
+            <ion-buttons>{this.getButtonElements()}</ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+      );
+    }
     return (
       <ion-header>
         <ion-toolbar color="dark">
-          <ion-buttons slot="end">
-            <ion-button href="/" padding>
-              Home
-            </ion-button>
-            <ion-button href="playground" padding>
-              Playground
-            </ion-button>
-            <ion-button href="about" padding>
-              About
-            </ion-button>
-          </ion-buttons>
+          <ion-buttons slot="end">{this.getButtonElements()}</ion-buttons>
         </ion-toolbar>
       </ion-header>
     );
+  }
+
+  getButtonElements() {
+    return [
+      <ion-button href="/" padding>
+        Home
+      </ion-button>,
+      <ion-button href="playground" padding>
+        Playground
+      </ion-button>,
+      <ion-button href="about" padding>
+        About
+      </ion-button>,
+    ];
   }
 }
