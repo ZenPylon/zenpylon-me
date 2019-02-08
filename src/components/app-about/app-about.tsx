1dac323945c30b33e252695a366766377fde1c98
Andrew Decker <ajdecker1022@gmail.com>
2019-02-08 12:25:19 -0600

    Add wellio link

--- src/components/app-about/app-about.tsx
+++ src/components/app-about/app-about.tsx
@@ -21,11 +21,12 @@ export class AppAbout {
           >
             <ion-col size-md="6" size-lg="5" size-xs="10">
               <p>
-                I’m a software engineer at Wellio. I do full-stack mobile and
-                web development using tools like Ionic, Angular, Firestore, and
-                Flask. Outside of work, I enjoy math, music, and meditation.
-                Much of my perspective is informed by my experience with Bipolar
-                II. I aspire to be grounded, compassionate, and perceptive.
+                I’m a software engineer at <a href="//getwellio.com"> Wellio</a>
+                . I do full-stack mobile and web development using tools like
+                Ionic, Angular, Firestore, and Flask. Outside of work, I enjoy
+                math, music, and meditation. Much of my perspective is informed
+                by my experience with Bipolar II. I aspire to be grounded,
+                compassionate, and perceptive.
               </p>
             </ion-col>
           </ion-row>
