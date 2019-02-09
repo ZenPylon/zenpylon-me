ff61382775d3cb43edba39b13362e6095a12ffce
Andrew Decker <ajdecker1022@gmail.com>
2019-02-09 09:52:41 -0600

    Add .content-row and lorem ipsum

--- src/components/app-playground/polynomial-sphere/polynomial-sphere.tsx
+++ src/components/app-playground/polynomial-sphere/polynomial-sphere.tsx
@@ -15,15 +15,33 @@ export class PolynomialSphere {
               <ion-col class="header-2">Polynomial Sphere</ion-col>
             </ion-row>
           </header>
-          <ion-row text-center justify-content-center align-items-center>
+          <ion-row
+            class="content-row"
+            text-center
+            justify-content-center
+            align-items-center
+          >
             <ion-col>
               <section>
-                <p>Explanation</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  dictum velit varius sem facilisis, ac tristique tellus
+                  condimentum. Ut ut quam convallis, efficitur enim et, laoreet
+                  erat. Vestibulum eu lobortis massa. Nullam dolor metus, tempor
+                  at neque vel, tristique porttitor quam. Pellentesque sit amet
+                  lorem laoreet orci sollicitudin rutrum. Nunc vel augue odio.
+                  Nulla ut mauris ac felis venenatis finibus sit amet sed nunc.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Mauris semper, enim vitae
+                  convallis finibus, leo elit tincidunt urna, a mollis quam urna
+                  eget nunc. Donec non fermentum nunc, at tempor sapien.
+                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                </p>
               </section>
             </ion-col>
             <ion-col>
               <section>
-                <p>canvas</p>
+                <p>Canvas</p>
               </section>
             </ion-col>
           </ion-row>
