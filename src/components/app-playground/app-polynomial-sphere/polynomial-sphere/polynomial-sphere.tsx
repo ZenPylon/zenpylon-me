c834f6aa9764ef249773c4786a7f3a1917c1049a
Andrew Decker <ajdecker1022@gmail.com>
2019-02-09 12:57:13 -0600

    Add polynomial-sphere properties

--- src/components/app-playground/app-polynomial-sphere/polynomial-sphere/polynomial-sphere.tsx
+++ src/components/app-playground/app-polynomial-sphere/polynomial-sphere/polynomial-sphere.tsx
@@ -1,10 +1,52 @@
 import { Component } from '@stencil/core';
+import {
+  Mesh,
+  MeshPhongMaterial,
+  MeshBasicMaterial,
+  Scene,
+  VertexColors,
+} from 'three';
+
+import { PolynomialSphereUtil } from './polynomial-sphere-util';
 
 @Component({
   tag: 'polynomial-sphere',
 })
 export class PolynomialSphere {
+  public colorMapName = 'rainbow';
+  public colorMapResolution = 512;
+  public mesh: Mesh;
+  public radius = 5;
+  public scene: Scene;
+
   render() {
     return <p>Sphere placeholder</p>;
+  }
+
+  private createSphere() {
+    if (this.mesh) {
+      return;
+    }
+
+    const icoSphere = PolynomialSphereUtil.createColoredIcoSphere(
+      this.radius,
+      4,
+      this.colorMapName,
+      this.colorMapResolution,
+    );
+    const material = new MeshPhongMaterial({
+      color: 0xffffff,
+      flatShading: true,
+      vertexColors: VertexColors,
+      shininess: 0,
+    });
+    const wireframeMaterial = new MeshBasicMaterial({
+      color: 0x000000,
+      wireframe: true,
+      transparent: true,
+    });
+    this.mesh = new Mesh(icoSphere.sphere, material);
+    this.mesh.position.x = 0;
+    this.scene.add(this.mesh);
   }
 }
