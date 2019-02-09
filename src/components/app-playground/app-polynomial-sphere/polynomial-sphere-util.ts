99762882d07dab421ae8fcb904af23f01ca1904b
Andrew Decker <ajdecker1022@gmail.com>
2019-02-09 12:09:56 -0600

    Change colormap name to pararmeter

--- src/components/app-playground/app-polynomial-sphere/polynomial-sphere-util.ts
+++ src/components/app-playground/app-polynomial-sphere/polynomial-sphere-util.ts
@@ -2,54 +2,42 @@ import { IcosahedronGeometry } from 'three';
 
 interface DiscriminantExtrema {
   min: number;
-  max: number
+  max: number;
 }
 export class PolynomialSphereUtil {
-  public static createIcoSphere(radius: number, detail: number, colorMap) {
+  public static createIcoSphere(radius: number, detail: number, colorMapName: string, colorMapResolution: number) {
     const icoSphere = new IcosahedronGeometry(radius, detail);
-    const extrema = this.getSphereExtrema(icoSphere, )_
-    const rainbowLut = new Lut('rainbow', 512);
-    rainbowLut.setMax(maxDiscriminant);
-    rainbowLut.setMin(minDiscriminant);
+    const extrema = this.getSphereExtrema(icoSphere);
+    const faceIndices = ['a', 'b', 'c'];
+    const colorMap = new Lut(colorMapName, colorMapResolution);
+    colorMap.setMax(extrema.max);
+    colorMap.setMin(extrema.min);
 
-    for (let i = 0; i < this.icosphere.faces.length; i++) {
-      f = this.icosphere.faces[i];
+    for (let i = 0; i < icoSphere.faces.length; i++) {
+      const f = icoSphere.faces[i];
 
       for (let j = 0; j < 3; j++) {
-        vertexIndex = f[faceIndices[j]];
-        p = this.icosphere.vertices[vertexIndex];
+        const vertexIndex = f[faceIndices[j]];
+        const p = icoSphere.vertices[vertexIndex];
         const coefA = p.x;
         const coefB = p.y;
         const coefC = p.z;
         const discriminant = Math.sqrt(
           Math.abs(coefB * coefB - 4 * coefA * coefC),
         );
-        f.vertexColors[j] = rainbowLut.getColor(discriminant);
+        f.vertexColors[j] = colorMap.getColor(discriminant);
       }
     }
-
-    const material = new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      flatShading: true,
-      vertexColors: THREE.VertexColors,
-      shininess: 0,
-    });
-    const wireframeMaterial = new THREE.MeshBasicMaterial({
-      color: 0x000000,
-      wireframe: true,
-      transparent: true,
-    });
-    const mesh = new THREE.Mesh(this.icosphere, material);
-    mesh.position.x = 0;
-    this.scene.add(mesh);
   }
 
-  private getSphereExtrema(icoSphere: IcosahedronGeometry): DiscriminantExtrema {
+  private static getSphereExtrema(
+    icoSphere: IcosahedronGeometry,
+  ): DiscriminantExtrema {
     // Go through and find the min / max values of the discriminant
     const faceIndices = ['a', 'b', 'c'];
     const extrema: DiscriminantExtrema = {
       min: null,
-      max: null
+      max: null,
     };
     let vertexIndex, f, p;
 
@@ -76,5 +64,24 @@ export class PolynomialSphereUtil {
 
       return extrema;
     }
+  }
+
+  private placeholder() {
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      flatShading: true,
+      vertexColors: THREE.VertexColors,
+      shininess: 0,
+    });
+    const wireframeMaterial = new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      wireframe: true,
+      transparent: true,
+    });
+    const mesh = new THREE.Mesh(this.icosphere, material);
+    mesh.position.x = 0;
+    this.scene.add(mesh);
+  }
+
   }
 }
