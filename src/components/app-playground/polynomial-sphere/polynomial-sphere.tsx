6eff34b01e1093d3f686b3a3da4882e84d5261d6
Andrew Decker <ajdecker1022@gmail.com>
2019-02-09 09:47:24 -0600

    Add placeholder sections to p-sphere

--- src/components/app-playground/polynomial-sphere/polynomial-sphere.tsx
+++ src/components/app-playground/polynomial-sphere/polynomial-sphere.tsx
@@ -15,6 +15,19 @@ export class PolynomialSphere {
               <ion-col class="header-2">Polynomial Sphere</ion-col>
             </ion-row>
           </header>
+          <ion-row text-center justify-content-center align-items-center>
+            <ion-col>
+              <section>
+                <p>Explanation</p>
+              </section>
+            </ion-col>
+            <ion-col>
+              <section>
+                <p>canvas</p>
+              </section>
+            </ion-col>
+          </ion-row>
+          <section />
         </ion-grid>
       </ion-content>,
     ];
