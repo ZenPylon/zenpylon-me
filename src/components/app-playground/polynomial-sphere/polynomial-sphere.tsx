254f06ca5651652b3827c8e1dee045acb7afb56b
Andrew Decker <ajdecker1022@gmail.com>
2019-02-08 18:47:09 -0600

    Add polynomial sphere stubs

---
+++ src/components/app-playground/polynomial-sphere/polynomial-sphere.tsx
@@ -0,0 +1,11 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'polynomial-sphere',
+  styleUrl: 'polynomial-sphere.scss',
+})
+export class PolynomialSphere {
+  render() {
+    return <p>Polynomial sphere</p>;
+  }
+}
