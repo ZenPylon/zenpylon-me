513db144705e21e239d67305c58836bbd554f792
Andrew Decker <ajdecker1022@gmail.com>
2019-02-22 23:14:53 -0600

    Add datasets to chart

--- src/components/app-playground/app-fourier-tracing/app-fourier-tracing.tsx
+++ src/components/app-playground/app-fourier-tracing/app-fourier-tracing.tsx
@@ -1,5 +1,5 @@
 import { Component, Element } from '@stencil/core';
-import Chart from 'chart.js';
+import { Chart, ChartDataSets } from 'chart.js';
 
 @Component({
   tag: 'app-fourier-tracing',
@@ -7,21 +7,37 @@ import Chart from 'chart.js';
 })
 export class AppPlayground {
   private mainFourierCanvas: HTMLCanvasElement;
+  // @ts-ignore
   private mainFourierChart: Chart;
-  private points: any = [
-    { x: 1, y: 1 },
-    { x: 2, y: 4 },
-    { x: 3, y: 6 },
-    { x: 4, y: 2 },
-    { x: 5, y: -2 },
-  ];
+  private templateData: ChartDataSets = {
+    label: 'Template Function',
+    data: [
+      { x: 1, y: 1 },
+      { x: 2, y: 4 },
+      { x: 3, y: 6 },
+      { x: 4, y: 2 },
+      { x: 5, y: -2 },
+    ],
+  };
   @Element() private element: HTMLElement;
 
   componentDidLoad() {
     this.mainFourierCanvas = this.element.querySelector('#main-fourier-plot');
     this.mainFourierChart = new Chart(this.mainFourierCanvas, {
       type: 'line',
-      data: this.points,
+      data: {
+        datasets: [this.templateData],
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: 'linear',
+              position: 'bottom',
+            },
+          ],
+        },
+      },
     });
   }
 
