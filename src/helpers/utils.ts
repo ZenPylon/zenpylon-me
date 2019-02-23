886ffdf74201ba39e4abe93c44cf3b894223ad56
Andrew Decker <ajdecker1022@gmail.com>
2019-02-22 23:48:01 -0600

    Add calculateFourierSeries, createLinearArray

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,20 @@
-export function sayHello() {
-  return Math.random() < 0.5 ? 'Hello' : 'Hola';
+export function calculateFourierSeries(
+  coefficients: number[],
+  xValues: number[],
+) {
+  if (coefficients.length !== xValues.length) {
+    throw Error('Coefficients and xValues must be of same length');
+  }
+  return coefficients.map((coefficient, index) => coefficient * xValues[index]);
+}
+
+export function createLinearArray(numElements: number) {
+  if (numElements < 1) {
+    throw Error('numElements must be greater than 0');
+  }
+  const values = Array(numElements);
+  for (let i = 0; i < numElements; i++) {
+    values[i] = i;
+  }
+  return;
 }
